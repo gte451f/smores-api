6bb179797055580671653412b07c4f1b4663bb86
jjenkins <jjenkins@smithcarson.com>
2015-03-20 20:40:03 -0400

    intial application with basic users table

---
+++ app/models/Users.php
@@ -0,0 +1,86 @@
+<?php
+namespace PhalconRest\Models;
+
+use Phalcon\Mvc\Model\Validator\Email as Email;
+
+class Users extends \PhalconRest\API\BaseModel
+{
+
+    /**
+     *
+     * @var integer
+     */
+    public $id;
+
+    /**
+     *
+     * @var string
+     */
+    public $first_name;
+
+    /**
+     *
+     * @var string
+     */
+    public $last_name;
+
+    /**
+     *
+     * @var string
+     */
+    public $email;
+
+    /**
+     *
+     * @var string
+     */
+    public $user_name;
+
+    /**
+     *
+     * @var string
+     */
+    public $password;
+
+    /**
+     *
+     * @var string
+     */
+    public $salt;
+
+    /**
+     * Validations and business logic
+     */
+    public function validation()
+    {
+
+        $this->validate(
+            new Email(
+                array(
+                    'field'    => 'email',
+                    'required' => true,
+                )
+            )
+        );
+        if ($this->validationHasFailed() == true) {
+            return false;
+        }
+    }
+
+    /**
+     * Independent Column Mapping.
+     */
+    public function columnMap()
+    {
+        return array(
+            'id' => 'id', 
+            'first_name' => 'first_name', 
+            'last_name' => 'last_name', 
+            'email' => 'email', 
+            'user_name' => 'user_name', 
+            'password' => 'password', 
+            'salt' => 'salt'
+        );
+    }
+
+}
