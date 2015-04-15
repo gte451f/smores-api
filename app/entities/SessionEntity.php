16566c9e20ccb5780c2f063591855ddce92359de
jjenkins <jjenkins@smithcarson.com>
2015-04-14 23:19:14 -0400

    add session and cabin resources

---
+++ app/entities/SessionEntity.php
@@ -0,0 +1,6 @@
+<?php
+namespace PhalconRest\Entities;
+
+class SessionEntity extends \PhalconRest\API\Entity
+{
+}
---
+++ app/models/Sessions.php
@@ -0,0 +1,46 @@
+<?php
+namespace PhalconRest\Models;
+
+class Sessions extends \PhalconRest\API\BaseModel
+{
+
+    /**
+     *
+     * @var integer
+     */
+    public $id;
+
+    /**
+     *
+     * @var string
+     */
+    public $start;
+
+    /**
+     *
+     * @var string
+     */
+    public $name;
+
+    /**
+     *
+     * @var string
+     */
+    public $end;
+
+    /**
+     * define custom model relationships
+     *
+     * (non-PHPdoc)
+     *
+     * @see extends \PhalconRest\API\BaseModel::initialize()
+     */
+    public function initialize()
+    {
+        parent::initialize();
+        
+        $this->hasMany("id", "PhalconRest\Models\Events", "program_id", array(
+            'alias' => 'Events'
+        ));
+    }
+}
